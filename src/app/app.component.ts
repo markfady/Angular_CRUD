45579ab3710880a187ec563a28742bfddea6cd2f
Markfady <markfady9@gmail.com>
2024-06-06 18:32:03 +0300

    app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `<div> {{pageTitle}}</div>`,
 })
 export class AppComponent {
-  title = 'Angular_CRUD';
+  pageTitle: string = 'Angular Getting Started';
 }
