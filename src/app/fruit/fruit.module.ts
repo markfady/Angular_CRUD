ef6c9126c121138c4ae15383936c288777d742e3
Markfady <markfady9@gmail.com>
2024-06-06 18:29:32 +0300

    setup fruit routing

--- src/app/fruit/fruit.module.ts
+++ src/app/fruit/fruit.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // Import RouterModule here
 
 import { FruitRoutingModule } from './fruit-routing.module';
 import { HomeComponent } from './home/home.component';
 import { CreateComponent } from './create/create.component';
 import { EditComponent } from './edit/edit.component';
-
 
 @NgModule({
   declarations: [
@@ -15,7 +15,12 @@ import { EditComponent } from './edit/edit.component';
   ],
   imports: [
     CommonModule,
-    FruitRoutingModule
+    FruitRoutingModule,
+    RouterModule.forRoot([
+      { path: 'fruit/index', component: HomeComponent },
+      { path: 'fruit',redirectTo: 'fruit', pathMatch: 'full' },
+      { path: '', redirectTo: 'fruit', pathMatch: 'full' } // Redirect to 'fruit' for the default route
+    ])
   ]
 })
 export class FruitModule { }
