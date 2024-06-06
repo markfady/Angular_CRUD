fae6425abfb8dc8ade36822f2b608f83830710ae
Markfady <markfady9@gmail.com>
2024-06-06 18:31:39 +0300

    import needed packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,23 @@
-    // Import Angular core modules and necessary dependencies
-    import { NgModule } from '@angular/core';
-    import { BrowserModule } from '@angular/platform-browser';
-    import { CommonModule } from '@angular/common';
-    import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // Import if you use template-driven forms
-    import { HttpClientModule } from '@angular/common/http'; // Import if you use HTTP services
-    // Import your main app component
-    import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClient, HttpClientModule} from '@angular/common/http'
+import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './fruit/home/home.component';
+import { CommonModule } from '@angular/common';
 
-    // Define the NgModule decorator to set up the module
-    @NgModule({
-    declarations: [
-        AppComponent, // Declare the main component and any other components used
-    ],
-    imports: [
-        
-        RouterModule,
-        BrowserModule, // Needed for any web-based app
-        FormsModule,   // Needed if you use template-driven forms
-        CommonModule,
-        ReactiveFormsModule,
-        HttpClientModule, // Needed if you use HTTP services
-      
-    ],
-    exports: [RouterModule],
-    providers: [ ],// Register services here, e.g. { provide: 'SomeService', useClass: SomeService }
-    bootstrap: [
-        AppComponent // The root component to bootstrap when the app starts
-    ]
-    })
-    export class AppModule { }
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
