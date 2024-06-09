98213c56a5235be8c7c56630c57fdec5f8de40c7
Markfady <markfady9@gmail.com>
2024-06-09 17:52:42 +0300

    increment id , create item method , initial state

--- src/app/fruit/create/create.component.ts
+++ src/app/fruit/create/create.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FruitService } from '../fruit.service';
+import { Router } from '@angular/router';
+import { Fruit } from '../fruit';
 
 @Component({
   selector: 'pm-create',
   templateUrl: './create.component.html',
-  styleUrl: './create.component.css'
+  styleUrls: ['./create.component.css']
 })
-export class CreateComponent {
+export class CreateComponent implements OnInit {
+  formdata: Fruit = {
+    id: 0, // Initialize with a numeric ID
+    name: '',
+    quantity: 0,
+    price: 0,
+  };
 
+  constructor(private fruitService: FruitService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.setNextId();
+  }
+
+  setNextId(): void {
+    this.fruitService.getAll().subscribe((items: Fruit[]) => {
+      if (items.length > 0) {
+        const maxId = Math.max(...items.map(item => item.id));
+        this.formdata.id = maxId + 1;
+      } else {
+        this.formdata.id = 1;
+      }
+    });
+  }
+
+  create(): void {
+    this.fruitService.create(this.formdata).subscribe({
+      next: (data) => {
+        this.router.navigate(['/fruit/home']);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
 }
