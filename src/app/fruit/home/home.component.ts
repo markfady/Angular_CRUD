e3fac567a22dd3da52fc99e5808424e4373ec232
Markfady <markfady9@gmail.com>
2024-06-09 14:59:33 +0300

    subscribe to the get service

--- src/app/fruit/home/home.component.ts
+++ src/app/fruit/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FruitService } from '../fruit.service';
+import { Fruit } from '../fruit';
 
 @Component({
   selector: 'pm-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  allFruits:Fruit[]=[]
+
+  constructor(private fruitService:FruitService){}
+
+  ngOnInit(): void {
+    this.fruitService.getAll().subscribe((data)=>{
+      this.allFruits=data
+    })
+  }
 }
